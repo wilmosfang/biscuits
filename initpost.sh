69faba104412984c31eaab0716adb29a15399545
wilmosfang <yyghdfz@163.com>
2017-11-07 01:16:49 +0800

    add analytics code add author img change url to siteurl add caculate js to footer change the css of title

--- initpost.sh
+++ initpost.sh
@@ -46,7 +46,8 @@ POSTPATH="${BINPATH}/_posts"
 DIST_FOLDER="$POSTPATH"
 
 # Set your blog URL
-BLOG_URL="https://willianjusten.com.br"
+#BLOG_URL="https://willianjusten.com.br"
+BLOG_URL="http://www.atomicgain.com"
 
 # Set your assets URL
 ASSETS_URL="assets/img/"
